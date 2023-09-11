809a1f47d730b3bd5e2fd3ab3680ea44e1178d66
Rafael Santos <rafa.05125@gmail.com>
2023-09-10 23:02:38 -0300

    ProductService at frontend - FindAll, FindById

--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceOnWebMicroservice/GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceOnWebMicroservice/GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -4,7 +4,7 @@ namespace GeekShopping.Web.Services.IServices
 {
     public interface IProductService
     {
-        Task<IEnumerable<ProductModel>> FindAlProducts();
+        Task<IEnumerable<ProductModel>> FindAllProducts();
         Task<ProductModel> FindProductById(long id);
         Task<ProductModel> CreateProduct(ProductModel model);
         Task<ProductModel> UpdateProduct(ProductModel model);
