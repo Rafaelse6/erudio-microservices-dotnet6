35bc5656f2c26c4d405473f8e0746e9c1f103ea8
Rafael Santos <rafa.05125@gmail.com>
2023-09-13 17:48:44 -0300

    Configuring clients at Identity Server

--- S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDDuendeAndIdentityServer/GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDDuendeAndIdentityServer/GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace GeekShopping.IdentityServer.Configuration
 {
@@ -24,7 +25,7 @@ namespace GeekShopping.IdentityServer.Configuration
             };
         public static IEnumerable<Client> Clients =>
               new List<Client>
-              {
+            {
                 new Client
                 {
                     ClientId = "client",
@@ -32,6 +33,21 @@ namespace GeekShopping.IdentityServer.Configuration
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = {"read", "write", "profile" }
                 },
-              };
+                new Client
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"http://localhost:36759/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:36759/signout-callback-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
+                }
+            };
     }
 }
