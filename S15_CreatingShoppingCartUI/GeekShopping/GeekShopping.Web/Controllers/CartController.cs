393e4535234e4bfdda9c3e05a8ada913f9178236
Rafael Santos <rafa.05125@gmail.com>
2023-09-28 21:46:49 -0300

    Cart View - Body

--- S15_CreatingShoppingCartUI/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
+++ S15_CreatingShoppingCartUI/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using GeekShopping.Web.Models;
 using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -15,6 +16,8 @@ namespace GeekShopping.Web.Controllers
             this._productService = productService;
             this._cartService = cartService;
         }
+
+        [Authorize]
         public async Task<IActionResult> CartIndex()
         {
             return View(await FindUserCart());
