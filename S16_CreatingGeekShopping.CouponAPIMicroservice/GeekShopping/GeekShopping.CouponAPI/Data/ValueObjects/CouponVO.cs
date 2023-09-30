12085795a7446c9f0d741485badfd2ee4f911ea3
Rafael Santos <rafa.05125@gmail.com>
2023-09-30 12:29:33 -0300

    Adding VO's to CouponAPI

---
+++ S16_CreatingGeekShopping.CouponAPIMicroservice/GeekShopping/GeekShopping.CouponAPI/Data/ValueObjects/CouponVO.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.CouponAPI.Data.ValueObjects
+{
+    public class CouponVO
+    {
+        public long id {  get; set; }
+
+        public string CouponCode { get; set; }
+
+        
+        public decimal DiscountAmount { get; set; }
+    }
+}
