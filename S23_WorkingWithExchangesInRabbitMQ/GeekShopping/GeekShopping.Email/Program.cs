f883cf60f2b834900bf37d85e96205d92386dc0a
Rafael Santos <rafa.05125@gmail.com>
2023-10-15 10:51:09 -0300

    Implementing messages, entities and context

--- S23_WorkingWithExchangesInRabbitMQ/GeekShopping/GeekShopping.Email/Program.cs
+++ S23_WorkingWithExchangesInRabbitMQ/GeekShopping/GeekShopping.Email/Program.cs
@@ -1,6 +1,16 @@
+using GeekShopping.Email.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 29)))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
